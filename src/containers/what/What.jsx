3b121a55f8569b8cf35bd811485dc3cd0a340789
Milky Raven <heartonllamas@gmail.com>
2022-12-11 20:28:55 +0100

    what finished

--- src/containers/what/What.jsx
+++ src/containers/what/What.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './what.css';
+import Feature from '../../components/feature/Feature' 
 
 function What() {
   return (
-    <div>What</div>
+    <div className='project__what section__margin' id="what">
+      <div className='project__what-feature'>
+        <Feature />
+      </div>
+      <div className='project__what-heading'>
+        <h1 className='gradient__text'>
+          The possibilities are beyond your imagination</h1>
+          <p>Explore the Library</p>
+      </div>
+      <div className='project__what-container'>
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
+    </div>
   )
 }
 
